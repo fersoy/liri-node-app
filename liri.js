e66a9fe850fbe5b2a654fa9afb6b9c1404874023
Fatih Ersoy <f.ersoytx@gmail.com>
2019-05-02 08:32:59 -0500

    files and needed codes are created

---
+++ liri.js
@@ -0,0 +1,29 @@
+
+require("dotenv").config();
+
+const keys = require("./keys.js");
+
+const ticketMaster = Ticketmaster(keys.ticketMaster);
+// $.ajax({
+//     type:"GET",
+//     url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=nBBTGNrN5In8KoHsLUOzEw2QlcyTVO64",
+//     async:true,
+//     dataType: "json",
+//     success: function(json) {
+//                 console.log(json);
+//                 // Parse the response.
+//                 // Do other things.
+//              },
+//     error: function(xhr, status, err) {
+//                 // This time, we do not end up here!
+//              }
+//   });
+
+
+`search-concerts`
+
+    // `search-songs`
+
+    // `search-movies`
+
+    // `feeling-lucky`
